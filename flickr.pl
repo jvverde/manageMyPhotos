7aa8a826ec546f24efe2fb198403a61296ff89c4
Isidro Vila Verde <jvverde@gmail.com>
2013-09-22 22:33:19 +0100

    check hash on flickr, before upload

--- flickr.pl
+++ flickr.pl
@@ -1,18 +1,39 @@
 #!/usr/bin/perl
 use strict;
 use Flickr::API;
-use Digest::MD5 qw|md5_hex|;
 use Data::Dumper;
 use Browser::Open qw|open_browser|;
 use Storable;
 use Flickr::Upload;
 use Cwd qw{abs_path};
 use Digest::SHA qw(sha256);
-
+use File::Copy qw(cp);
+use XML::XPath;
+use Getopt::Long;
 
 $\ = "\n";
 
-my $msg = q|Now goto to your browser and in the new opened window give authorization, close that window (or the browser) and then return here and enter C to continue|;
+my $sync = undef;
+my $auth = undef;
+my $stop = undef;
+sub tsktsk {
+    $SIG{INT} = \&tsktsk;
+    print "OK! I will stop soon";
+    $stop = 1;
+}
+$SIG{INT} = \&tsktsk;
+
+GetOptions ('sync|s' => \$sync, 'auth|a' => \$auth);
+
+@ARGV >=3 or die qq|Usage $0 [options] profile sourceDir  pattern [tag1 [tag2 ... [tagN]]]|;
+
+my ($profile, $dir, $pattern, @tags) = @ARGV;
+
+my $msg = q|Now goto to your browser and
+	1-Give Authorization in the new opened window
+	2-Close that window (or the browser)
+	3-Return here
+	4-Enter C to (C)ontinue|;
 
 my $api_key = '19b47264d5e18d50962ac56345510fbc';
 my $shared_secret = '9f01bc4567657508';
@@ -22,14 +43,18 @@ my $api = new Flickr::API({
 	secret => $shared_secret
 });
 
-my $hashfile="config.hash";
+my $hashfile = qq|.$profile.profile|;
 store {}, $hashfile unless -r $hashfile;
-my $hash=retrieve($hashfile);
-print join(",", %$hash), "\n";
+my $hash = retrieve($hashfile) or die 'Corrupted profile';
+cp $hashfile, qq|$hashfile.old|;
+#print Dumper $hash;
+#print join(",", %$hash), "\n";
 $hash->{cnt}++;
 store $hash, $hashfile;
 
-while(! defined $hash->{auth_token}){
+undef $hash->{auth_token} if $auth;
+
+while(! defined $hash->{auth_token} || ! defined $hash->{nsid}){
 	my $permission_wanted = 'write';
 	print 'Get frob...';
 	my $response = $api->execute_method('flickr.auth.getFrob');
@@ -42,7 +67,8 @@ while(! defined $hash->{auth_token}){
 		print $msg;
 		#print $url;
 		open_browser($url);
-		my $ctl = <>;
+		print "Enter C to continue";
+		my $ctl = <STDIN>;
 		chomp $ctl;
 		last if $ctl =~ /^c$/i;
 	};
@@ -52,28 +78,71 @@ while(! defined $hash->{auth_token}){
 	});
 	print 'Check answer...';
 	my $r = $response->decoded_content(charset => 'none');
+	print $r;
 	warn qq|Warning:\n$r| and next unless $r =~ /<rsp[^>]+stat\s*=\s*"ok".*>/i;
 	print 'Extract token...';
 	$r =~ /<token>(.+)<\/token>/ or die qq|ERROR:\n$r|;
 	my $token = $1;
 	print 'Save token...';
 	$hash->{auth_token} = $token;
+	$r =~ /<user\s+nsid\s*=\s*"([^"]+)"\s+username\s*=\s*"([^"]+)"\s+fullname\s*=\s*"([^"]+)"/
+		or die qq|ERROR:\n$r|;
+	$hash->{nsid} = $1;
+	$hash->{username} = $2;
+	$hash->{fullname} = $3;
 	store $hash, $hashfile;
 };
 
-@ARGV >=2 or die qq|Usage $0 sourceDir  pattern [tag1 [tag2 ... [tagN]]]|;
+syncFromFlickr() if ($sync);
+getFolders($dir);
 
-my ($dir, $pattern, @tags) = @ARGV;
 
+sub syncFromFlickr{
+	my $page = shift || 1;
+	my $response = $api->execute_method('flickr.photos.search', {
+		user_id => $hash->{nsid},
+		auth_token => $hash->{auth_token},
+	  #machine_tags => "meta:id=",
+	  extras => 'machine_tags',
+		per_page => 500,
+		page => $page
+	});
+	warn $response->status_line and return unless $response->is_success;
+	my $answer  = $response->decoded_content(charset => 'none');
+	my $xp = XML::XPath->new(xml => $answer);
+	warn qq|Wrong answer:\n\t$answer|
+		and return if $xp->getNodeText('/rsp/@stat')->value ne 'ok';
+  my $npages = $xp->getNodeText('/rsp/photos/@pages')->value;
+  my $npage = $xp->getNodeText('/rsp/photos/@page')->value;
+	print "Sync page $npage of $npages";
+  my @photos = $xp->find('/rsp/photos/photo')->get_nodelist();
+  foreach (@photos){
+  	#print Dumper $_;
+  	my $xp = XML::XPath->new(context => $_);
+  	my $photoid = $xp->getNodeText('@id')->value;
+  	my $mt = $xp->getNodeText('@machine_tags')->value;
+  	$mt =~ /meta:id\s*=\s*([0-9a-f]+)/i;
+  	my $id = $1;
+  	unless($hash->{ids}->{$id}){
+  		print "$id does not exists in local cache";
+	  	$hash->{ids}->{$id} = $photoid;
+  	}
+  }
+  store $hash, $hashfile;
+  exit if $stop;
+  syncFromFlickr($page+1) if $page < $npages;
+}
 
 sub getFolders{
 	my $dir = shift;
+	exit if $stop;
 	print qq|Get dir $dir|;
 	if ($dir =~ $pattern){
 		backup($dir,@tags);
 	}
 	getSubFolders($dir);
 }
+
 sub getSubFolders{
 	my $dir = shift;
 	opendir DIR, $dir or warn qq|Nao foi possivel abrir o directorio $dir| and return;
@@ -92,49 +161,71 @@ sub backup{
 	my @files = grep {/\.(jpg|nef)$/i} readdir DIR;
 	foreach (@files){
 		my $path = abs_path(qq|$dir/$_|);
-		my $id = getFileID($path);
-		next if defined $hash->{ids}->{$id};
-		my @localtags = (
-			qq|dir:path="$path"|,
-			qq|meta:id="$id"|,
-			qq|file:name="$_"|,
-			map {qq|dir:step="$_"|} grep {/[^\s]/} split /\//, $path
-		);
-		pop @localtags; #discard filename from dir:step tags
 		eval{
-			upload($path,$id,@localtags,@tags);
+			upload($path,@tags);
 		};
 		warn $@ if $@;
+		exit if $stop;
 	}
-
-
 }
-sub getFileID{
-        warn(q|getFileID: File not defined|) and return undef unless defined $_[0];
-        warn(q|getFileID: File not $_[0] found|) and return undef unless -e $_[0];
-        my $sha = Digest::SHA->new();
-        $sha->addfile($_[0],'b');
-        return $sha->hexdigest;
-}
-
-my $ua = Flickr::Upload->new({
-	key => $api_key,
-	secret => $shared_secret
-});
 
 sub upload{
-	my ($file,$id, @tags) = @_;
+	my ($file,@tags) = @_;
+	my $ua = Flickr::Upload->new({
+		key => $api_key,
+		secret => $shared_secret
+	});
 	#print "Upload file $file";
-	my $photoid = $ua->upload(
-		photo => $file,
-		auth_token => $hash->{auth_token},
-		tags => (join ' ', @tags),
-		is_public => 0,
-		is_private => 1
-	) or warn "Failed to upload $file" and return;
-	$hash->{ids}->{$id} = $photoid;
-	print "File $file uploaded to $photoid";
+	my $mtime =  (stat($file))[9];
+	return if $hash->{filenae} eq $mtime;
+	my $id = getFileID($file);
+	return if defined $hash->{ids}->{$id};
+	if (checkFlickrPhoto($id) == 0){ #if not yet on flickr upload
+		my @localtags = (
+			qq|dir:path="$file"|,
+			qq|meta:id="$id"|,
+			q|time:modification="|.localtime($mtime).q|"|,
+			map {qq|dir:step="$_"|} grep {/[^\s]/} split /\//, $file
+		);
+		pop @localtags; #discard filename from dir:step tags
+
+
+		my $photoid = $ua->upload(
+			photo => $file,
+			auth_token => $hash->{auth_token},
+			tags => (join ' ', @tags, @localtags),
+			is_public => 0
+		) or warn "Failed to upload $file" and return;
+		print "File $file uploaded to flickr (photoid = $photoid)";
+		$hash->{ids}->{$id} = $photoid;
+	}else{
+		print "File $file ($id) is already on flickr";
+		$hash->{ids}->{$id} = 0;
+	}
+	$hash->{filename} = $mtime;
 	store $hash, $hashfile;
 }
 
-getFolders($dir);
+sub getFileID{
+	warn(q|getFileID: File not defined|) and return undef unless defined $_[0];
+	warn(q|getFileID: File not $_[0] found|) and return undef unless -e $_[0];
+	my $sha = Digest::SHA->new();
+	$sha->addfile($_[0],'b');
+	return $sha->hexdigest;
+}
+
+sub checkFlickrPhoto{
+	my $id = shift;
+	#print "Check photo $id";
+	my $response = $api->execute_method('flickr.photos.search', {
+		user_id => $hash->{nsid},
+		auth_token => $hash->{auth_token},
+	  machine_tags => qq|meta:id="$id"|
+	});
+	my $answer  = $response->decoded_content(charset => 'none');
+	my $xp = XML::XPath->new(xml => $answer);
+	warn qq|Wrong answer:\n\t$answer|
+		and return undef if $xp->getNodeText('/rsp/@stat')->value ne 'ok';
+  my $nphotos = $xp->getNodeText('/rsp/photos/@total')->value;
+  return $nphotos;
+}
